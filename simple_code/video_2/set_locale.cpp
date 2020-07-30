d288007f0564664fa29871fe1dffabd9a56ff33e
Artak <artakkirakosyan96@mail.ru>
2020-07-30 13:01:30 +0400

    Added first 2 videos of SimpleCode tutorials. Added Readme.

---
+++ simple_code/video_2/set_locale.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    // this is a comment
+ 
+    /* this is also a comment
+     * but multiline
+     */
+    setlocale(LC_ALL, "Rus");
+    cout << "Hello world!" << endl;
+}
+
