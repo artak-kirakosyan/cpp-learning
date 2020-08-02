17c2080bc8c7debb637ab530bc3748ce77ad7ed2
Artak <artakkirakosyan96@mail.ru>
2020-08-02 21:40:34 +0400

    Added videos 4 and 5.

---
+++ simple_code/video_5/declaration.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // declare an integer without an initial value
+    int a;
+    // assign 5 to the variable
+    a = 5;
+    // print out
+    cout << a << endl;
+
+    // declare an integer with initial value 5
+    int b = 5;
+    cout << b << endl;
+
+    int k = 1, c = 12;
+    
+    cout << k << endl;
+    cout << c << endl;
+
+}
+
